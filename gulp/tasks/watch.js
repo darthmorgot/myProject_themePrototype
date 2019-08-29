cc7cd7ac25560dc6759097a23eec7214d7e9d7ac
darthmorgot <dartmorgot@yandex.ru>
2019-08-30 01:32:15 +1000

    Добавил новый файл в папку tasks watch.js

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,15 @@
+const {watch} = require('gulp');
+
+const pug = require('./pug');
+const imagesDev = require('./imagesDev');
+const scripts = require('./scripts');
+
+function watcher() {
+	watch('./dev/pug/**/*.pug', pug);
+	// watch('./dev/static/styles/**/*.scss', stylesDev);
+	watch('./dev/static/images/**/*.{png,jpg,gif,svg}', imagesDev);
+	// watch('./dev/static/images/svg/*.svg', svg);
+	watch('./dev/static/js/**/*.js', scripts);
+};
+
+module.exports = watcher;
