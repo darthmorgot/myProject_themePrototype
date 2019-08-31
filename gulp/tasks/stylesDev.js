31f9348c51ec2e354a6ea547e947cfa97c7682d1
darthmorgot <dartmorgot@yandex.ru>
2019-09-01 01:08:33 +1000

    Добавил новый файл в папку tasks stylesDev.js

---
+++ gulp/tasks/stylesDev.js
@@ -0,0 +1,25 @@
+const {src, dest} = require('gulp');
+
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+const	scss = require('gulp-sass');
+const	sourcemaps = require('gulp-sourcemaps');
+const	autoprefixer = require('gulp-autoprefixer');
+
+function styles() {
+	return src('./dev/static/styles/styles.scss')
+		.pipe(plumber({
+			errorHandler: notify.onError(err => ({
+				title: 'Error when compiling styles',
+				message: err.message
+			}))
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(scss())
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write())
+		.pipe(dest('./build/css'))
+};
+
+module.exports = styles;
