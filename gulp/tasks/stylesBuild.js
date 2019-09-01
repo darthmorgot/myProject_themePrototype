1f5862d735d61276726b3fff9ab4889ac8a09154
darthmorgot <dartmorgot@yandex.ru>
2019-09-01 22:39:12 +1000

    Добавил новый файл в папку tasks stylesBuild.js

---
+++ gulp/tasks/stylesBuild.js
@@ -0,0 +1,28 @@
+const {src, dest} = require('gulp');
+
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+const	scss = require('gulp-sass');
+const	autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+
+function stylesBuild() {
+	return src('./dev/static/styles/styles.scss')
+		.pipe(plumber({
+			errorHandler: notify.onError(err => ({
+				title: 'Error when compiling styles',
+				message: err.message
+			}))
+		}))
+		.pipe(scss())
+		.pipe(autoprefixer())
+		.pipe(cleanCSS({
+			level: 2
+		}))
+		.pipe(rename('styles.min.css'))
+		.pipe(dest('./build/css'))
+};
+
+module.exports = stylesBuild;
