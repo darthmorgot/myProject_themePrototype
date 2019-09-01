675191f4d6ce015531943e6d032d2facea3104e9
darthmorgot <dartmorgot@yandex.ru>
2019-09-01 22:46:31 +1000

    Обновил код в файле pug.js

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -5,10 +5,13 @@ const notify = require('gulp-notify');
 
 const	pug = require('gulp-pug');
 const changed = require('gulp-changed');
+const gulpIf = require('gulp-if');
+const cached = require('gulp-cached');
 const pugInheritance = require('gulp-pug-inheritance');
+const filter = require('gulp-filter');
 
 function pugCompile() {
-	return src('./dev/pug/*.pug')
+	return src('./dev/pug/**/*.pug')
 		.pipe(plumber({
 			errorHandler: notify.onError(err => ({
 				title: 'Error when compiling pug',
@@ -16,7 +19,11 @@ function pugCompile() {
 			}))
 		}))
 		.pipe(changed('./build', {extension: '.html'}))
+		.pipe(gulpIf(global.isWatching, cached('pug')))
 		.pipe(pugInheritance({basedir: './dev/pug/', skip: 'node_modules'}))
+		.pipe(filter(function (file) {
+			return !/\/_/.test(file.path) && !/^_/.test(file.relative);
+		}))
 		.pipe(pug({
 			pretty: true
 		}))
