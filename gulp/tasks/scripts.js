ef209c9091b9537117987b5edb9bc395dbf9796e
darthmorgot <dartmorgot@yandex.ru>
2019-09-10 00:22:37 +1000

    Обновил код в файле scripts.js

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,6 +2,25 @@ const {src, dest} = require('gulp');
 
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const webpack = require('webpack-stream');
+
+let isDev = true;
+let isProd = !isDev;
+
+let webConfig = {
+	mode: isDev ? 'development' : 'production',
+	devtool: isDev ? 'eval-source-map' : 'none',
+	output: {
+		filename: 'index.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: '/node_modules/'
+		}]
+	}
+};
 
 function scripts() {
 	return src('./dev/static/js/index.js')
@@ -11,6 +30,7 @@ function scripts() {
 				message: err.message
 			}))
 		}))
+		.pipe(webpack(webConfig))
 		.pipe(dest('./build/js'));
 }
 
