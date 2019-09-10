58b90f16dc5d2cc051f0317b477752c67d36b66a
darthmorgot <dartmorgot@yandex.ru>
2019-09-10 23:56:23 +1000

    Обновил код в файле watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,15 +2,16 @@ const {watch} = require('gulp');
 
 const pug = require('./pug');
 const imagesDev = require('./imagesDev');
-const scripts = require('./scripts');
+const scriptsDev = require('./scriptsDev');
 const stylesDev = require('./stylesDev');
+const svg = require('./svg');
 
 function watcher() {
 	watch('./dev/pug/**/*.pug', pug);
 	watch('./dev/static/styles/**/*.scss', stylesDev);
 	watch('./dev/static/images/**/*.{png,jpg,gif,svg}', imagesDev);
-	// watch('./dev/static/images/svg/*.svg', svg);
-	watch('./dev/static/js/**/*.js', scripts);
+	watch('./dev/static/images/svg/*.svg', svg);
+	watch('./dev/static/js/**/*.js', scriptsDev);
 };
 
 module.exports = watcher;
